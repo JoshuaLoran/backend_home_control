b3f81e609ebab6a1809e2f7c06110a26f4ffb643
Joshua Loran <joshua.loran@gmail.com>
2019-05-16 09:19:16 -0700

    action cable set up

---
+++ app/channels/devices_channel.rb
@@ -0,0 +1,16 @@
+class DevicesChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from 'devices'
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def recieve(data)
+    device = Device.find(data["id"])
+    device.update!(commands: data["commands"])
+    ActionCable.server.broadcast('devices', data)
+  end
+end
